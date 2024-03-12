f0d913cee0c313bd79dcd4cd1c1680177c417a9e
portkey-yellow <yellow.m@portkey.finance>
2024-03-12 14:50:57 +0800

    fix: 🐛 lib url

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -25,7 +25,6 @@ export interface Options {
     signal?: AbortSignal;
     /** @default false */
     preserveExif?: boolean;
-    /** @default https://cdn.jsdelivr.net/npm/browser-image-compression/dist/browser-image-compression.js */
     libURL?: string;
 }
 
